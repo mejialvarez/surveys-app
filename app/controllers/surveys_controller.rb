ca15924413ddd0c36693711ad97b2ef89e39d45b
Hamilton Alvarez <alvarezhamilton@gmail.com>
2020-10-01 15:33:09 -0500

    Add authentication

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,4 +1,5 @@
 class SurveysController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
   def index
     @surveys = Survey.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'surveys#index'
 
+  devise_for :users
+
   resources :surveys do
     post 'complete', on: :member
     get 'summary', on: :member
