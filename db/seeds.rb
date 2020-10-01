171d28497bcd66f40a78391acdfc347572cb6951
Hamilton Alvarez <alvarezhamilton@gmail.com>
2020-10-01 16:02:13 -0500

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+s = Survey.new(title: 'Survey example')
+s.save
+
+s.questions.create(title: 'Overall, how satisfied or dissatisfied are you with our company?',
+    answer_options: [
+      'Very satisfied',
+      'Somewhat satisfied',
+      'Very dissatisfied']
+    )
+
+s.questions.create(title: 'Which of the following words would you use to describe our products? Select all that apply.',
+    answer_options: [
+      'Reliable',
+      'Hight quality',
+      'Unique']
+    )
+
